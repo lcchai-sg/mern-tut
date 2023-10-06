d24cb12667ab60986d8370bd5ba64afb78d334ed
lcchai <lc.chai.sg@gmail.com>
2023-10-06 14:21:09 +0800

    completed register & login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -26,6 +26,26 @@ export const register = createAsyncThunk("register", async (user, thunkAPI) => {
     }
 });
 
+// Login user
+export const login = createAsyncThunk("login", async (user, thunkAPI) => {
+    try {
+        return await authService.login(user);
+    } catch (error) {
+        console.log(error);
+        const message =
+            (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+            error.message ||
+            error.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
+export const logout = createAsyncThunk("/logout", async () => {
+    await authService.logout();
+});
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -48,6 +68,24 @@ export const authSlice = createSlice({
                 state.user = action.payload;
             })
             .addCase(register.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = false;
+                state.isError = true;
+                state.message = action.payload;
+                state.user = null;
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null;
+            })
+            .addCase(login.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.user = action.payload;
+            })
+            .addCase(login.rejected, (state, action) => {
                 state.isLoading = false;
                 state.isSuccess = false;
                 state.isError = true;
